8493f3316cb7ad3dad57d20fb226b6cf9ebb3935
Priyanshu Agarkar <20102046.priyanshu.agarkar@gmail.com>
2022-03-21 19:05:04 +0530

    day 4

--- lib/main1.dart
+++ lib/main1.dart
@@ -1,4 +1,7 @@
+import 'package:basics_of_flutter/pages/loginpage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'pages/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +13,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 30 days of flutter"),
-          ),
-        ),
+      // home: Homepage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
       ),
     );
   }
